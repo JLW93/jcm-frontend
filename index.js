8d6dd9c5a1a8ea2c37a8aa510a5dd56bcc05cb9d
JLW93 <wornerjl@gmail.com>
2023-06-28 11:11:48 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,92 @@
+const galleryImgs = document.querySelectorAll('.gallery-img');
+const modal = document.querySelector('.modal');
+const modalImg = document.querySelector('.modal-content');
+const closeButton = document.querySelector('.close');
+const backDrop = document.querySelector('.modal-overlay');
+
+for (let img of galleryImgs) {
+    img.addEventListener('click', () => {
+        modal.style.display = 'block';
+        modalImg.src = img.src;
+    });
+}
+
+closeButton.addEventListener('click', closeModal)
+
+backDrop.addEventListener('click', closeModal)
+
+function closeModal() {
+    modal.style.display = 'none';
+}
+
+function toServices() {
+    let e = document.getElementById('services');
+    e.scrollIntoView({
+        block: 'start',
+        behavior: 'smooth',
+        inline: 'center'
+    });
+}
+
+function toGallery() {
+    let e = document.getElementById('gallery');
+    e.scrollIntoView({
+        block: 'start',
+        behavior: 'smooth',
+        inline: 'center'
+    });
+}
+
+function toContact() {
+    let e = document.getElementById('contact');
+    e.scrollIntoView({
+        block: 'start',
+        behavior: 'smooth',
+        inline: 'center'
+    });
+}
+
+function toTop() {
+    let e = document.getElementById('nav');
+    e.scrollIntoView({
+        block: 'start',
+        behavior: 'smooth',
+        inline: 'center'
+    });
+}
+
+const form = document.querySelector('#contactForm');
+// const url = 'https://jlw93-automatic-broccoli-6qj4wx9r9r6c5q4p-5500.preview.app.github.dev/jcm-backend';
+const port = 3000;
+const url = `http://127.0.0.1`
+
+form.addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.target);
+    const selectedServices = Array.from(formData.getAll('type_of_service'));
+    const data = Object.fromEntries(formData.entries());
+
+    Object.assign(data, {type_of_service: selectedServices});
+
+    try {
+        const response = await fetch(`${url}:${port}/api/data/email`, { // replace '/api/data' with url for backend API
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Allow': 'POST'
+            },
+            body: JSON.stringify(data),
+        });
+
+        if (response.ok) {
+            // success code, maybe an alert to let the user know that the message sent?
+            alert('Message sent! We will contact you soon!')
+        } else {
+            // error code, maybe try to give different errors depending on the situation?
+            alert('Message failed to send. Please try again.')
+        }
+    } catch (error) {
+        // error code for network or other errors
+    }
+})
